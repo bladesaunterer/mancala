c026c8e6a6ffc91d37290d5ca856110354928d8c
Carson Lourenco <carson.lourenco101@gmail.com>
2017-06-08 20:07:59 +1200

    Tests almost pass

--- src/kalah/board/AClockwiseTraversal.java
+++ src/kalah/board/AClockwiseTraversal.java
@@ -2,6 +2,8 @@ package kalah.board;
 
 public class AClockwiseTraversal implements KalahTraversable {
 	public KalahTraversalState getNextTraversalState(KalahTraversalState currentTraversalState, int housesPerPlayer){
+		Player currentPlayersTurn = currentTraversalState.getCurrentPlayerHoldSeedsDistributed();
+		Player otherPlayer = (currentPlayersTurn == Player.ONE) ? Player.TWO : Player.ONE;
 		int currentSeedContainer = currentTraversalState.getCurrentSeedContainerBeingSewnForPlayer();
 		Player playerSewingSeeds = currentTraversalState.getCurrentPlayerHoldSeedsDistributed();
 		
@@ -13,6 +15,11 @@ public class AClockwiseTraversal implements KalahTraversable {
 		}else{
 			currentSeedContainer++;
 		}
+		
+//		if(playerSewingSeeds == otherPlayer && currentSeedContainer==housesPerPlayer){
+//			currentSeedContainer = 0;
+//			playerSewingSeeds = currentPlayersTurn;
+//		}
 		
 		return new KalahTraversalState(playerSewingSeeds, currentSeedContainer);
 		
--- src/kalah/board/StandardKalahVariant.java
+++ src/kalah/board/StandardKalahVariant.java
@@ -15,9 +15,11 @@ public class StandardKalahVariant extends KalahVariantGameLogic {
 	public Player playerTurn(PlayerHold playerOneHold, PlayerHold playerTwoHold, int houseSelection,
 			Player currentPlayer, KalahTraversable boardTraverser) {
 		boolean captureOccurs = false;
+		int seedsToDistribute = 0;
 
 		if(currentPlayer == Player.ONE){
 			int seeds = playerOneHold.removeSeedsFromHouse(houseSelection);
+			seedsToDistribute = seeds;
 			if(seeds == 0){
 				KalahOutput.pleaseTryAgain(io);
 				return currentPlayer;
@@ -47,17 +49,16 @@ public class StandardKalahVariant extends KalahVariantGameLogic {
 				
 				// TODO checking if capture occurs, triggered by fact that last seed is being sewn - factor this out
 				// should be dependent on opposite house and if it is a valid thing to steal from capture rule remains consistent
-				if(seeds == 1
-						&& currentSeedContainer != housesPerPlayer
-						&& playerSewingSeeds == playerOneHold
-						&& playerSewingSeeds.getNumOfSeedsInHouse(currentSeedContainer+1) == 1
-						&& playerTwoHold.getNumOfSeedsInHouse(housesPerPlayer - currentSeedContainer) > 0) {
-					captureOccurs = true;
-				}
+//				if(seeds == 1
+//						&& currentSeedContainer != housesPerPlayer
+//						&& playerSewingSeeds == playerOneHold
+//						&& playerSewingSeeds.getNumOfSeedsInHouse(currentSeedContainer+1) == 1
+//						&& playerTwoHold.getNumOfSeedsInHouse(housesPerPlayer - currentSeedContainer) > 0) {
+//					captureOccurs = true;
+//				}
 				
 				//determine where seeds distributed next
 				KalahTraversalState traversalState = new KalahTraversalState((playerSewingSeeds == playerOneHold) ? Player.ONE : Player.TWO,currentSeedContainer);
-				//KalahTraverser traverser = new  Traverser() traversal = traverser.getNextState(traversal)
 
 				traversalState = boardTraverser.getNextTraversalState(traversalState, housesPerPlayer);
 				
@@ -69,7 +70,9 @@ public class StandardKalahVariant extends KalahVariantGameLogic {
 			}
 			
 			//perform steal
-			if(captureOccurs){
+			Player lastToSewSeed = (currentPlayer == Player.ONE) ? Player.TWO : Player.ONE;
+			
+			if(captureOccurs(lastToSewSeed, playerOneHold,playerTwoHold,houseSelection,seedsToDistribute, housesPerPlayer)){
 				int capturedSeeds = playerTwoHold.removeSeedsFromHouse(housesPerPlayer-currentSeedContainer+1);
 				int seedAtLastIndex = playerOneHold.removeSeedsFromHouse(currentSeedContainer);
 				
@@ -78,6 +81,7 @@ public class StandardKalahVariant extends KalahVariantGameLogic {
 			
 		} else {
 			int seeds = playerTwoHold.removeSeedsFromHouse(houseSelection);
+			seedsToDistribute = seeds;
 			if(seeds == 0){
 				KalahOutput.pleaseTryAgain(io);
 				return currentPlayer;
@@ -105,22 +109,22 @@ public class StandardKalahVariant extends KalahVariantGameLogic {
 				
 				playerSewingSeeds.incrementSeedsInHouse(currentSeedContainer+1);
 				
-				if(seeds == 1
-						&& currentSeedContainer != housesPerPlayer
-						&& playerSewingSeeds == playerTwoHold
-						&& playerSewingSeeds.getNumOfSeedsInHouse(currentSeedContainer+1) == 1
-						&& playerOneHold.getNumOfSeedsInHouse(housesPerPlayer - currentSeedContainer) >0
-						) {
-					captureOccurs = true;
-				}
+//				if(seeds == 1
+//						&& currentSeedContainer != housesPerPlayer
+//						&& playerSewingSeeds == playerTwoHold
+//						&& playerSewingSeeds.getNumOfSeedsInHouse(currentSeedContainer+1) == 1
+//						&& playerOneHold.getNumOfSeedsInHouse(housesPerPlayer - currentSeedContainer) >0
+//						) {
+//					captureOccurs = true;
+//				}
+				
+
 			
 				//Change KalahTraversal State, set player sewing seeds, change current seedContainer
 				
 				
 				
 				KalahTraversalState traversalState = new KalahTraversalState((playerSewingSeeds == playerOneHold) ? Player.ONE : Player.TWO,currentSeedContainer);
-				//KalahTraverser traverser = new  Traverser() traversal = traverser.getNextState(traversal)
-
 				traversalState = boardTraverser.getNextTraversalState(traversalState, housesPerPlayer);
 				
 				playerSewingSeeds = (traversalState.getCurrentPlayerHoldSeedsDistributed() == Player.ONE) ? playerOneHold:playerTwoHold;
@@ -132,9 +136,9 @@ public class StandardKalahVariant extends KalahVariantGameLogic {
 			}
 			
 			
-			
+			Player lastToSewSeed = (currentPlayer == Player.ONE) ? Player.TWO : Player.ONE;
 		
-			if(captureOccurs){
+			if(captureOccurs(lastToSewSeed, playerOneHold,playerTwoHold,houseSelection,seedsToDistribute, housesPerPlayer)){
 				int capturedSeeds = playerOneHold.removeSeedsFromHouse(housesPerPlayer-currentSeedContainer+1);
 				int seedAtLastIndex = playerTwoHold.removeSeedsFromHouse(currentSeedContainer);
 				playerTwoHold.addToPlayerStore(capturedSeeds+seedAtLastIndex);
@@ -149,23 +153,119 @@ public class StandardKalahVariant extends KalahVariantGameLogic {
 	
 	
 	
-	private boolean captureOccurs(PlayerHold currentPlayer, int houseSelection, int seedsPriorToDistribution, int housesPerPlayer){
-		//determine if last seed placed on this side
-		boolean lastSeedPlacedInThisPlayersHolding = true;
+	private boolean captureOccurs(Player currentPlayer, PlayerHold pOneHold, PlayerHold pTwoHold, int houseSelection, int seedsPriorToDistribution, int housesPerPlayer){
 		
-		if(houseSelection + seedsPriorToDistribution <= housesPerPlayer ) {
-			lastSeedPlacedInThisPlayersHolding = true;
+		KalahTraversable traverser = new AClockwiseTraversal();
+		KalahTraversalState stateAfterDistribution = new KalahTraversalState(currentPlayer, houseSelection);
+		int seeds = seedsPriorToDistribution;
+		boolean captureOccurs = false;
+		
+		System.out.println(seeds);
+		
+		int numOfLoops = seeds/(housesPerPlayer*2+1);
+		seeds += numOfLoops;
+		
+		while(seeds > 0) {
+			stateAfterDistribution = traverser.getNextTraversalState(stateAfterDistribution, housesPerPlayer);
+			PlayerHold currentSeedHoldSewn = (stateAfterDistribution.getCurrentPlayerHoldSeedsDistributed() == Player.ONE) ? pOneHold : pTwoHold;
+			PlayerHold otherPlayer = (stateAfterDistribution.getCurrentPlayerHoldSeedsDistributed() == Player.ONE) ? pTwoHold : pOneHold;
+			
+			
+			
+			if(seeds == 1) {
+//				
+				
+//				
+//				System.out.println(stateAfterDistribution.getCurrentPlayerHoldSeedsDistributed());
+//				System.out.println(stateAfterDistribution.getCurrentSeedContainerBeingSewnForPlayer());
+				
+				
+				if(stateAfterDistribution.getCurrentPlayerHoldSeedsDistributed() != currentPlayer) {
+					
+					return false;
+				}
+				
+//				int houseLastSewn = stateAfterDistribution.getCurrentSeedContainerBeingSewnForPlayer();
+//				if(houseLastSewn < housesPerPlayer 
+//						&& currentSeedHoldSewn.getNumOfSeedsInHouse(houseLastSewn+1) == 0
+//						&& otherPlayer.getNumOfSeedsInHouse(housesPerPlayer - stateAfterDistribution.getCurrentSeedContainerBeingSewnForPlayer()+1) > 0)
+//					return true;
+				
+				int houseOne = stateAfterDistribution.getCurrentSeedContainerBeingSewnForPlayer();
+				int otherHouse = housesPerPlayer - stateAfterDistribution.getCurrentSeedContainerBeingSewnForPlayer()+1;
+				if(houseOne == 0 || houseOne == housesPerPlayer+1 ){
+					
+					return false;
+				}else {
+//					System.out.println("reached");
+					System.out.println(currentSeedHoldSewn.getNumOfSeedsInHouse(houseOne));
+					System.out.println(otherPlayer.getNumOfSeedsInHouse(otherHouse));
+					return (currentSeedHoldSewn.getNumOfSeedsInHouse(houseOne) ==1 
+							&& otherPlayer.getNumOfSeedsInHouse(otherHouse) >0 );
+				}
+			}
+
+			
+			seeds--;
 		}
 		
-		if(seedsPriorToDistribution + houseSelection > 1  )
-		
-		if(!lastSeedPlacedInThisPlayersHolding) return lastSeedPlacedInThisPlayersHolding;
 		
 		
+		return captureOccurs;
 		
 		
 		
-		return false;
+		
+		
+		
+//		Player playerToLastDistributeSeed = stateAfterDistribution.getCurrentPlayerHoldSeedsDistributed();
+//		int seedContainerDistributed = stateAfterDistribution.getCurrentSeedContainerBeingSewnForPlayer();
+//		System.out.println(stateAfterDistribution.getCurrentPlayerHoldSeedsDistributed());
+//		System.out.println(seedContainerDistributed);
+//		System.out.println(housesPerPlayer - seedContainerDistributed +1);
+//		
+//		if(playerToLastDistributeSeed != currentPlayer)
+//			return false;
+//		
+//		if(seedContainerDistributed <= housesPerPlayer){
+//			if(currentPlayer == Player.ONE){
+//				captureOccurs  = pOneHold.getNumOfSeedsInHouse(seedContainerDistributed) == 1 && pTwoHold.getNumOfSeedsInHouse(housesPerPlayer - seedContainerDistributed +1) >0;
+//				System.out.println(captureOccurs);
+//				return captureOccurs;
+//			}else {
+//				captureOccurs = pTwoHold.getNumOfSeedsInHouse(seedContainerDistributed) == 1 && pOneHold.getNumOfSeedsInHouse(housesPerPlayer - seedContainerDistributed +1) >0;
+//				System.out.println(captureOccurs);
+//				return captureOccurs;
+//			}
+//		}
+//		
+//		
+//		return false;
+		
+		
+
+		
+		
+//		if(playerToLastDistributeSeed == Player.ONE 
+//				&& pOneHold.getNumOfSeedsInHouse(seedContainerDistributed) == 1
+//				&& seedContainerDistributed != housesPerPlayer){
+//			captureOccurs = pTwoHold.getNumOfSeedsInHouse(housesPerPlayer - seedContainerDistributed) > 0;
+//		}else if (seedContainerDistributed != housesPerPlayer
+//				&& pTwoHold.getNumOfSeedsInHouse(seedContainerDistributed) == 1) {
+//			captureOccurs = pOneHold.getNumOfSeedsInHouse(housesPerPlayer - seedContainerDistributed) > 0;
+//			
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		return captureOccurs;
 		
 	}
 	
